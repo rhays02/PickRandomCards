3bf745f9ad82510f580332c756d271536d1249b8
Robert Hays <roberthays@hotmail.com>
2021-02-21 12:47:10 -0500

    finished first exercise of app

--- PickRandomCards/Program.cs
+++ PickRandomCards/Program.cs
@@ -9,6 +9,22 @@ namespace PickRandomCards
             Console.Write("Enter the number of cards to pick:  ");
 
             string line = Console.ReadLine();
+
+            if (int.TryParse(line, out int numberOfCards))
+            {
+                // this block is executed if line COULD be converted to an int
+                // value that's stored in the new variable called numberOfCards
+                foreach (string card in CardPicker.PickSomeCards(numberOfCards))
+                {
+                    Console.WriteLine(card);
+                }
+
+            }
+            else
+            {
+                //this block is executed if line COULD NOT be converted to an int
+                Console.WriteLine("The number entered was invalid");
+            }
         }
     }
 }
