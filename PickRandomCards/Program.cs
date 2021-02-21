f9fe78d5b3ed0d378eb1fc1a8bf52c63edadaa4b
Robert Hays <roberthays@hotmail.com>
2021-02-21 12:19:01 -0500

    Add project files.

---
+++ PickRandomCards/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick:  ");
+
+            string line = Console.ReadLine();
+        }
+    }
+}
